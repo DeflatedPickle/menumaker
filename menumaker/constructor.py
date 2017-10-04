049e72bef9fff955c4e22a2700c06cdfb4e6ca45
Dibbo <DeflatedPickle@gmail.com>
2017-10-04 14:14:22 +0100

    Automatically detects functions with label names

--- menumaker/constructor.py
+++ menumaker/constructor.py
@@ -5,7 +5,7 @@
 import tkinter as tk
 
 __title__ = "Constructor"
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 __author__ = "DeflatedPickle"
 
 
@@ -14,15 +14,26 @@ def constructor(parent: tk.Menu, menus: dict, title: bool=True):
         # print("Menu:", menu)
         tkmenu = tk.Menu(parent)
         for item in menus["menus"][menu]:
+            # print("Item:", item)
             for command in menus["menus"][menu]["items"]:
-                # print("Item:", item)
-                tkmenu.add_command(label=command if not title else command.title())
+                print("Command:", command)
+                tkmenu.add_command(label=command if not title else command.title(), command=_set_command(command))
 
         parent.add_cascade(label=menu if not title else menu.title(), menu=tkmenu)
         # print("-----")
 
 
+def _set_command(command):
+    try:
+        return getattr(__import__(__name__), command)
+
+    except AttributeError:
+        return None
+
+
 if __name__ == "__main__":
+    def new():
+        print("New!")
 
     root = tk.Tk()
     menu = tk.Menu(root)
