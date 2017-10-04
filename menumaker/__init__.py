ef3d94135145cd6dc2d037aaba599f29f8a872fa
Dibbo <DeflatedPickle@gmail.com>
2017-10-04 10:35:54 +0100

    Added menu creation

--- menumaker/__init__.py
+++ menumaker/__init__.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""""
+
 from .constructor import constructor
+
+__title__ = "menumaker"
+
+__author__ = "DeflatedPickle/Dibbo"
+__copyright__ = "Copyright (c) 2017 Dibbo"
+__credits__ = ["DeflatedPickle/Dibbo"]
+
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "DeflatedPickle/Dibbo"
+__email__ = "DeflatedPickle@gmail.com"
+__status__ = "Development"
--- menumaker/constructor.py
+++ menumaker/constructor.py
@@ -1,20 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""""""
+
 import tkinter as tk
 
+__title__ = "Constructor"
+__version__ = "1.0.1"
+__author__ = "DeflatedPickle"
 
-def constructor(parent: tk.Tk, menus: dict):
+
+def constructor(parent: tk.Menu, menus: dict):
     for menu in menus["menu"]:
-        print("Menu", menu)
+        # print("Menu:", menu)
+        tkmenu = tk.Menu(parent)
         for item in menus["menu"][menu]:
-            print("Item", item)
+            # print("Item:", item)
+            tkmenu.add_command(label=item)
 
-        print("---")
+        parent.add_cascade(label=menu, menu=tkmenu)
+        # print("-----")
 
 
 if __name__ == "__main__":
 
     root = tk.Tk()
-    constructor(root, {"menu": {
+    menu = tk.Menu(root)
+
+    constructor(menu, {"menu": {
         "file": ["new", "open", "save"],
         "edit": ["undo", "redo"]
     }})
+
+    root.configure(menu=menu)
     root.mainloop()
