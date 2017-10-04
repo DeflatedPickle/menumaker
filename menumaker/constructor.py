ba06ab2c55874e5d8eb17e5a4fa5343e035b5ef0
Dibbo <DeflatedPickle@gmail.com>
2017-10-04 10:42:57 +0100

    Added the option to title names

--- menumaker/constructor.py
+++ menumaker/constructor.py
@@ -5,19 +5,19 @@
 import tkinter as tk
 
 __title__ = "Constructor"
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __author__ = "DeflatedPickle"
 
 
-def constructor(parent: tk.Menu, menus: dict):
-    for menu in menus["menu"]:
+def constructor(parent: tk.Menu, menus: dict, title: bool=True):
+    for menu in menus["menus"]:
         # print("Menu:", menu)
         tkmenu = tk.Menu(parent)
-        for item in menus["menu"][menu]:
+        for item in menus["menus"][menu]:
             # print("Item:", item)
-            tkmenu.add_command(label=item)
+            tkmenu.add_command(label=item if not title else item.title())
 
-        parent.add_cascade(label=menu, menu=tkmenu)
+        parent.add_cascade(label=menu if not title else menu.title(), menu=tkmenu)
         # print("-----")
 
 
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     menu = tk.Menu(root)
 
-    constructor(menu, {"menu": {
+    constructor(menu, {"menus": {
         "file": ["new", "open", "save"],
         "edit": ["undo", "redo"]
     }})
