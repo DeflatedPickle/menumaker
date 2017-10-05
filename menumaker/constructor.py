b8a318e55b48e8fddd68d9c7eaebe38d8a934676
DeflatedPickle <DeflatedPickle@gmail.com>
2017-10-05 03:00:57 +0100

    Improved bracket checking

--- menumaker/constructor.py
+++ menumaker/constructor.py
@@ -6,7 +6,7 @@ import tkinter as tk
 from collections import OrderedDict
 
 __title__ = "Constructor"
-__version__ = "1.7.0"
+__version__ = "1.8.1"
 __author__ = "DeflatedPickle"
 
 
@@ -25,11 +25,11 @@ def constructor(parent: tk.Menu, menus: dict, title: bool=True, auto_functions: 
                 if command == "---":
                     tkmenu.add_separator()
 
-                elif "[]" in command:
-                    tkmenu.add_checkbutton(label=title.replace("[]", ""))
+                elif "[" in command and "]" in command:
+                    tkmenu.add_checkbutton(label=_remove_brackets(title, "[]"), variable=_check_variable(command, "[]"))
 
-                elif "()" in command:
-                    tkmenu.add_radiobutton(label=title.replace("()", ""))
+                elif "(" in command and ")" in command:
+                    tkmenu.add_radiobutton(label=_remove_brackets(title, "()"), variable=_check_variable(command, "()"))
 
                 elif "-" in command:
                     tkmenu.add_cascade(label=title.replace("-", ""), menu=all_menus[command])
@@ -50,6 +50,12 @@ def _set_command(command):
     except AttributeError:
         return None
 
+def _check_variable(string: str, brackets: str):
+    return string[string.index(brackets[0]):string.index(brackets[1]) + 1]
+
+def _remove_brackets(string: str, brackets: str):
+    return string[string.index(brackets[1]) + 1:]
+
 
 if __name__ == "__main__":
     def new():
@@ -58,11 +64,14 @@ if __name__ == "__main__":
     root = tk.Tk()
     menu = tk.Menu(root)
 
+    var = tk.IntVar()
+    var2 = tk.IntVar()
+
     constructor(menu, {"menus": OrderedDict([
         ("file", {"items": ["new", "open", "save"]}),
         ("edit", {"items": ["undo", "redo", "---", "cut", "copy", "paste"]}),
-        ("-background", {"items": ["()green", "()red"]}),
-        ("view", {"items": ["[]toolbar", "-background"]})
+        ("-background", {"items": ["(var)green", "(var)red"]}),
+        ("view", {"items": ["[var2]toolbar", "-background"]})
     ])})
 
     root.configure(menu=menu)
