cd9e94338c78116eade09b946013db0c708368b0
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-12 00:57:20 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="menumaker",
-      version="1.2.0",
+      version="1.2.1",
       description="An easy solution for creating menus for TkInter applications in just a few lines.",
       author="DeflatedPickle",
       author_email="DeflatedPickle@gmail.com",
